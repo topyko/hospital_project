5f4ce99e1963f4023dd08479ba7f7e693a50cf6e
mpalei <mpalei@lebay.com>
2021-04-02 15:03:56 +0300

    added give_treatment and generate_report

--- emergency/doctor.py
+++ emergency/doctor.py
@@ -1,15 +1,34 @@
-from emergency.staff_member import  StaffMember
+from emergency.staff_member import StaffMember
+from emergency.adult import Adult
+from emergency.child import Child
+import numpy as np
 
 
 class Doctor(StaffMember):
     def __init__(self,  first_name, last_name, presonal_id, age, licence_number):
         super(Doctor, self).__init__(first_name, last_name, presonal_id, age, licence_number)
 
-    def generate_report(self):
-        pass
+    @staticmethod
+    def generate_report():
+        report = {
+                "Total ratio of recovered COVID patients": None,
+                "Ratio of COVID recovered children": None,
+                "Ratio of COVID recovered senior citizens (above 65)": None
+                  }
+        return report
 
-    def generate_final_report(self):
-        pass
+    @staticmethod
+    def generate_final_report():
+        return {}
 
-    def give_treatment(self):
-        pass
+    @staticmethod
+    def give_treatment(patients):
+        outcome = [0, 1]
+        for patient in patients:
+            if isinstance(patient, Adult):
+                weights = [0.7, 0.3]
+            elif isinstance(patient, Child):
+                weights = [0.6, 0.4]
+            success = np.choices(outcome, weights)
+            if success == 1:
+                patient.diag = None
