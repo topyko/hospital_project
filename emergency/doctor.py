2b8e3588943badf5baf8e374cc60a77b8fcdff61
topyko <55323160+topyko@users.noreply.github.com>
2021-04-02 18:14:38 +0300

    added test

--- emergency/doctor.py
+++ emergency/doctor.py
@@ -3,24 +3,34 @@ from emergency.adult import Adult
 from emergency.child import Child
 from emergency.patient import Patient
 import numpy as np
+from datetime import datetime
 
 
 class Doctor(StaffMember):
     def __init__(self,  first_name, last_name, presonal_id, age, licence_number):
         super(Doctor, self).__init__(first_name, last_name, presonal_id, age, licence_number)
 
-    # @staticmethod
-    # def generate_report(patients:list[Patient]):
-    #     report = {
-    #             "Total ratio of recovered COVID patients": None,
-    #             "Ratio of COVID recovered children": None,
-    #             "Ratio of COVID recovered senior citizens (above 65)": None
-    #               }
-    #     return report
-    #
-    # @staticmethod
-    # def generate_final_report():
-    #     return {}
+    @staticmethod
+    def generate_report(patients:list[Patient]):
+        positive = list(filter(lambda x: x.diag == 'Positive', patients))
+        recovered = list(filter(lambda x: x.diag == 'Recovered', patients))
+        positive_children = list(filter(lambda x: x.diag == 'Positive' and x.age <= 16, patients))
+        recovered_children = list(filter(lambda x: x.diag == 'Recovered' and x.age <= 16, patients))
+        positive_senior = list(filter(lambda x: x.diag == 'Positive' and x.age > 65, patients))
+        recovered_senior = list(filter(lambda x: x.diag == 'Recovered' and x.age > 65, patients))
+        report = {
+                "Total ratio of recovered COVID patients": len(recovered)/(len(positive)+len(recovered)),
+                "Ratio of COVID recovered children": len(recovered_children)/(len(positive_children)+len(recovered_children)),
+                "Ratio of COVID recovered senior citizens (above 65)": len(recovered_senior)/(len(positive_senior)+len(recovered_senior))
+                  }
+        return report
+
+    @staticmethod
+    def generate_final_report(patients:list[Patient]):
+        report = {}
+        for p in patients:
+            report[datetime.now()] = p.personal_data
+        return report
 
     @staticmethod
     def give_treatment(patients):
@@ -32,4 +42,4 @@ class Doctor(StaffMember):
                 weights = [0.6, 0.4]
             success = np.choices(outcome, weights)
             if success == 1:
-                patient.diag = None
+                patient.diag = 'Recovered'
