476f5e6c55a9764b7b64d01a7a1b0ce0280bb624
mpalei <mpalei@lebay.com>
2021-04-02 15:05:18 +0300

    added requires_isolation field

--- emergency/patient.py
+++ emergency/patient.py
@@ -5,6 +5,7 @@ class Patient(Person):
         # super(Patient, self).__init__(first_name, last_name, presonal_id, age)
         self.body_temperature, self.symptoms = body_temperature, symptoms
         self.diag = None
+        self.requires_isolation = False
 
     # # @personal_data.setter
     # def personal_data(self, val):
